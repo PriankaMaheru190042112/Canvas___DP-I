c625046707f4c65be003a38e49ffff56fc0eab26
ajwad-abrar <ajwadabrar@iut-dhaka.edu>
2022-09-08 11:43:48 +0600

    Added template folder and rendered index html page

--- code/canvas/base/views.py
+++ code/canvas/base/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Starting the project")
+    # return HttpResponse("Starting the project")
+    return render(request, 'index.html')
