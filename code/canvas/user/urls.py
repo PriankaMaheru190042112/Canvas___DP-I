eb3c19476b9952b108aa3424d84c09e010140139
ajwad-abrar <ajwadabrar@iut-dhaka.edu>
2022-10-14 01:46:59 +0600

    Added User Home Template Page

--- code/canvas/user/urls.py
+++ code/canvas/user/urls.py
@@ -7,4 +7,5 @@ app_name = 'user'
 
 urlpatterns = [
       path('user_auth/',views.user_auth , name='user_auth'),  
+      path('user_home/',views.user_home , name='user_home'),  
 ]
--- code/canvas/user/views.py
+++ code/canvas/user/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def user_auth(request):
     # return HttpResponse("Starting the project")
     return render(request, 'user/user_auth.html') 
+
+
+def user_home(request):
+    # return HttpResponse("Starting the project")
+    return render(request, 'user/user_home.html') 
